320cf459da1bf897e1a0128c1c0cfbd666321f02
oZwZo <zhengwzh5@hotmail.com>
2024-07-26 18:06:26 +0800

    Notebook branch : mk_ery_ad density

--- Explore_Notebook/density/denmarf_estimate.py
+++ Explore_Notebook/density/denmarf_estimate.py
@@ -3,17 +3,17 @@ from denmarf import DensityEstimate
 import numpy as np
 
 
-save_dir = "/home/wergillius/Project/TIGON/Density/denmarf_pickle"
+save_dir = "/ssd/users/Wergillius/Project/PINN_dynamics/Explore_Notebook/density"
 
 
-DM_ay = np.load('/home/wergillius/Project/TIGON/Input/clu7_DiffMap.npy', allow_pickle=True)
+DM_ay = np.load('/ssd/users/Wergillius/Project/PINN_dynamics/data/mkery_DM10.npy', allow_pickle=True)
 timepoints = [ 3,   7,  12,  27,  49,  76, 112, 161, 269]
 
 
 for i, dm in enumerate(DM_ay):    
-    de = DensityEstimate(device='cuda:0', use_cuda=True).fit(dm, num_epochs=500)
+    de = DensityEstimate(device='cuda:2', use_cuda=True).fit(dm, p_train=1, num_epochs=500)
     de.save(f"{save_dir}/DiffMap30_day{timepoints[i]}th.pkl")
 
 for i, dm in enumerate(DM_ay):    
-    de = DensityEstimate(device='cuda:0', use_cuda=True).fit(dm[:,:10], num_epochs=500)
-    de.save(f"{save_dir}/DiffMap10_day{timepoints[i]}th.pkl")
+    de = DensityEstimate(device='cuda:2', use_cuda=True).fit(dm[:,:5], p_train=1, num_epochs=500)
+    de.save(f"{save_dir}/DiffMap5_day{timepoints[i]}th.pkl")
--- Explore_Notebook/density/plotting_fn.py
+++ Explore_Notebook/density/plotting_fn.py
@@ -27,7 +27,7 @@ def umap_by_time(color_col, anndata, timepoints=timepoints):
         axis_j += 1
 
     return fig, axs
-def plot_along_pseudotime(color_col, anndata, timepoints=timepoints):
+def plot_along_pseudotime(color_col, anndata, pt_col='dpt_pseudotime', timepoints=timepoints):
     
     fig,axs = plt.subplots(2, 9, figsize=(30,4), dpi=100, gridspec_kw={'hspace':0.3})
     # axs = axs.flatten()
@@ -44,7 +44,7 @@ def plot_along_pseudotime(color_col, anndata, timepoints=timepoints):
 
         axs[0,axis_j].invert_xaxis()
 
-        sns.kdeplot(anndata.obs.query('`timepoint_tx_days` == @t')['dpt_pseudotime_scaled'], label="d%d"%t, ax=axs[1,axis_j])
+        sns.kdeplot(anndata.obs.query('`timepoint_tx_days` == @t')[pt_col], label="d%d"%t, ax=axs[1,axis_j])
         sns.despine(ax=axs[1,axis_j])
         axs[1,axis_j].set_xlim(0,1)
         axs[1,axis_j].set_ylabel("")
@@ -58,13 +58,13 @@ def plot_along_pseudotime(color_col, anndata, timepoints=timepoints):
     # axs[-1].axis("off")
     return fig
 
-def scatter_density(color_col, anndata, timepoints=timepoints):
+def scatter_density(color_col, anndata, pt_col='dpt_pseudotime', timepoints=timepoints):
     fig,axs = plt.subplots(1, 9, figsize=(30,2), dpi=100, gridspec_kw={'wspace':0.3})
     # axs = axs.flatten()
     axis_j = 0
 
     for t in timepoints:
         cbs = anndata.obs.query('`timepoint_tx_days` == @t').index
-        sns.scatterplot(data=anndata.obs.loc[cbs], x='dpt_pseudotime_scaled', y = color_col, ax=axs[axis_j])
+        sns.scatterplot(data=anndata.obs.loc[cbs], x=pt_col, y = color_col, ax=axs[axis_j])
         axis_j += 1
     return fig
