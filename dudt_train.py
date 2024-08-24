8fa74abdca125a2c2772f60b35a98fc9681b9e92
oZwZo <zhengwzh5@hotmail.com>
2024-08-25 05:52:05 +0800

    scripts for training pde integrated models

---
+++ dudt_train.py
@@ -0,0 +1,136 @@
+import os, argparse, typing
+
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import torch 
+from torch import nn
+from torch import optim
+from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler
+from functools import partial
+import pytorch_lightning as pl
+from pytorch_lightning import callbacks 
+from pytorch_lightning import loggers as pl_loggers
+
+import PINN
+from PINN import models as models
+from PINN import reader 
+from PINN import functions as fns
+
+
+torch.set_float32_matmul_precision('medium')
+
+    
+
+parser = argparse.ArgumentParser("Training PINN dynamics on single timepoint dataset")
+parser.add_argument("-D", "--dataset", type=str, required=False, default="HSPC_clu7", help='the name of the dataset, can be found under folder data')
+parser.add_argument("-K", "--cellstate_key", type=str, required=False, default="cellstate", help='the obsm key on which we represent cell and compute density')
+parser.add_argument("-M", "--model", type=str, required=False, default="Cspline_PINN", help='the model class, defined in models.py')
+parser.add_argument("-W", "--pretrained", type=str, required=False, default=None, help='the path of the pretrained weights')
+parser.add_argument("-G", "--gpu_devices", type=int, required=True, default=None, help='select which gpu devices to use')
+parser.add_argument("--lr", type=float, required=False, default=3e-3, help='the learning rate for training the model')
+parser.add_argument("--schedule_lr", type=str, required=False, default="StepLR", help='LambdaLR if passing a lambda expression, else StepLR')
+parser.add_argument("--n_grid", type=int, required=False, default=300, help='the number of grid or h to devid the cell state space')
+parser.add_argument("--n_dimension", type=int, required=False, default=5, help='the number of dimension to used for estimating density')
+parser.add_argument("--n_timepoint", type=int, required=False, default=7, help='the number of time point to train the model')
+parser.add_argument("--batch_size", type=int, required=False, default=200, help='the number of nearby cell state to include within a minibatch')
+parser.add_argument("--channels", type=str, required=False, default="3,32,32,1", help='the depth and width of the model')
+parser.add_argument("--time_sensitive", action="store_true", required=False, help='Whether to include time in behavoir functions')
+args = parser.parse_args()
+
+# args = parser.parse_args([
+#             "-D", "ery_mk", "-K", "DM_scaled",  "-M", "MLP_PINN", "-G", "6", "--n_timepoint", "5", 
+#             "--batch_size", "50", "--channels", "6,32,32,1", "--n_dimension", "6", "--schedule_lr", "CyclicLR",
+#             "--lr", "1e-4", "--n_timepoint" ,"7"])
+
+
+                                ###               ###
+                                #     read data     #
+                                ###               ###
+
+path = os.path.abspath(".")
+h5_path = os.path.join(path, f'{args.dataset}.h5ad')
+
+if not os.path.exists(h5_path):
+    main_path = path
+    h5_path = os.path.join(path, f'data/{args.dataset}.h5ad')
+else:
+    main_path = os.path.dirname(path)
+
+
+save_path = os.path.join(main_path, 'logs', f"{args.dataset}-{args.cellstate_key}_n{args.n_timepoint}", args.model+['','_tsense'][args.time_sensitive])
+if not os.path.exists(save_path):
+    os.mkdir(save_path)
+
+
+adata = sc.read_h5ad(h5_path)
+
+# define model
+model_class = eval(f"models.{args.model}")
+channels = [int(c) for c in args.channels.split(",")]   
+
+# for g v and D
+if args.model == "pde_params": 
+    n_dim = args.n_dimension + 1 if args.time_sensitive else args.n_dimension
+    model_kws = dict(v_channels = [n_dim, 128, 32, args.n_dimension],
+                    g_channels = [n_dim, 128,32,1],
+                    D_channels = [n_dim, 32,32,1])
+else:
+    model_kws = {}
+
+
+if args.pretrained is not None:
+    model = model_class.load_from_checkpoint(args.pretrained)
+else:
+    model = model_class(
+        lr=3e-4,
+        channels = channels,
+        activation_fn='Tanh',
+        **model_kws
+    )
+
+train_DS = reader.TwoTimpepoint_AnnDS(
+                            AnnData=adata, 
+                            n_timepoint=args.n_timepoint,  
+                            n_dimension = args.n_dimension,
+                            cellstate_key=args.cellstate_key,  #'DM_EigenVector'
+                            log_transform=False,
+                            norm_time=False,
+                            batchsize=args.batch_size)
+
+def my_collection_fn(batch):
+
+    s, (t, t_p1), (u_t, u_tp1) = batch
+
+    s.require_grad_(True)
+    t.require_grad_(True)
+    t_p1.require_grad_(True)
+    u_t.require_grad_(True)
+    u_tp1.require_grad_(True)
+
+    return s, (t, t_p1), (u_t, u_tp1)
+
+train_DL = DataLoader(train_DS, batch_size=None, num_workers=20)
+# train_iter = iter(train_DL)
+# s, (t, t_p1), (u_t, u_tp1) = next(train_iter)
+
+
+
+device = 'gpu' if torch.cuda.is_available() else 'cpu'
+device = 'cpu' if args.gpu_devices == None else 'gpu'
+gpu_device = args.gpu_devices
+
+trainer = pl.Trainer(
+                    #auto_lr_find=True,
+                    accelerator=device,
+                    # fast_dev_run=True,
+                    # gradient_clip_val=0.5,
+                    default_root_dir=save_path,
+                    devices = [gpu_device], 
+                    max_epochs=3000,
+                    callbacks=[callbacks.ModelCheckpoint(filename='{epoch}-{total_loss:.8f}',
+                                                monitor="total_loss", mode="min", save_top_k=2)]
+                    )
+
+trainer.fit(model, train_dataloaders=train_DL)
