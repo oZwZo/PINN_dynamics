1d3509fa02714f063c3f1dd13f312348af568f10
oZwZo <zhengwzh5@hotmail.com>
2024-10-01 23:56:44 +0800

    update the notebooks

--- Explore_Notebook/6.simulation/6-5.pde_params_eval.py
+++ Explore_Notebook/6.simulation/6-5.pde_params_eval.py
@@ -13,21 +13,22 @@ from torchdiffeq import odeint
 os.chdir("/ssd/users/Wergillius/Project/PINN_dynamics")
 
 # CHANG THIS !!!!!
-ckpt_path = "logs/TwoTimpepoint_u_dt_weight_n7/lightning_logs/version_0/checkpoints/epoch=28-total_loss=0.54973233 copy.ckpt"
+ckpt_path = "logs/tom_pos-DM_scaled_n9/pde_params_tsense/lightning_logs/version_1/checkpoints/epoch=174-total_loss=0.88114280.ckpt"
 
-model_name = ckpt_path.split("/")[1].replace("TwoTimpepoint_","").replace("_n7","")
+model_name = ckpt_path.split("/")[2].replace("_tsense","")
 model_class = eval(f"models.{model_name}")
 
 # MODEL CLASS and define model
 pde_model = model_class.load_from_checkpoint(ckpt_path)
 device = pde_model.device
 
-adata = sc.read_h5ad(f"data/tom_pos.h5ad")
+data_name = ckpt_path.split("/")[1].split("-")[0]
+adata = sc.read_h5ad(f"data/{data_name}.h5ad")
 timepoints = adata.uns['pop']['t']
 
-cellstate_key = "DM_scaled"
+cellstate_key = ckpt_path.split("/")[1].split("-")[1].split("_n")[0]
 
-n_timepoint = 7
+n_timepoint = int(ckpt_path.split("/")[1].split("-")[1].split("_n")[1])
 n_dimension =10
 
 # the DS
@@ -43,7 +44,21 @@ t7_ad = DS_t7.adata.copy()
 u_b = DS_t7.u_b.cpu().numpy().reshape(n_timepoint, -1)
 cellstate = torch.from_numpy(DS_t7.cellstate).float().requires_grad_()
 
+if "u" in dir(pde_model):
+    u_pred_ls = []
+    chunk_size= 500
 
+    s_ts = DS_t7.s.float().to(device)
+    t_ts = DS_t7.t_b.float().to(device)
+
+    with torch.no_grad():
+        for i in tqdm(range(0, len(t_ts), chunk_size)):
+            u_pred = pde_model(s_ts[i:i+chunk_size],  t_ts[i:i+chunk_size])
+            u_pred_ls.append(u_pred.detach().cpu().numpy())
+    u_pred_b = np.concatenate(u_pred_ls, axis=0).reshape(n_timepoint,-1)
+
+    PINN.pl.params_in_umap(t7_ad, u_b, param='u', cell_of_t=False);
+    PINN.pl.params_in_umap(t7_ad, u_pred_b, param='u pred by forward', cell_of_t=False);
 
 ##
 ## predict u with ode integrat
@@ -51,11 +66,11 @@ u_int_all = []
 chunk_size= 500
 # t_list = timepoints / 15
 it = 0
-for t0, t1 in zip(timepoints[:-1], timepoints[1:]):
+for t0, t1 in tqdm(zip(timepoints[:-1], timepoints[1:])):
 
     u_t_ls = []
 
-    for i in tqdm(range(0, len(cellstate), chunk_size)):
+    for i in range(0, len(cellstate), chunk_size):
 
         s0 = cellstate[i:i+chunk_size].to(device)
         tompos_u0 = torch.from_numpy(u_b[it, i:i+chunk_size]).float().to(device).requires_grad_()
@@ -69,7 +84,7 @@ for t0, t1 in zip(timepoints[:-1], timepoints[1:]):
         u_t, s_t = odeint(
                         pde_model.ode_func,
                         init_condition,
-                        torch.tensor([t0, t1]).type(torch.float32).to(device),
+                        torch.tensor([t0, t1]).type(torch.float32).to(device)/15,
                         atol=1e-8,
                         rtol=1e-8,
                         method='midpoint',
@@ -78,11 +93,11 @@ for t0, t1 in zip(timepoints[:-1], timepoints[1:]):
 
         torch.cuda.empty_cache()
 
-        u_t_ls.append(u_t.detach().cpu().numpy())
+        u_t_ls.append(u_t[-1].detach().cpu().numpy())
         del u_t, s_t
         torch.cuda.empty_cache()
 
-    u_int = np.concatenate(u_t_ls, axis=1)
+    u_int = np.concatenate(u_t_ls)
     
     u_int_all.append(u_int)
     it+=0
